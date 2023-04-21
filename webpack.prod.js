ca2e226108bc2473bb1014af2f82f64bbdc9b078
Romain Bruckert <adadgio@users.noreply.github.com>
2023-04-21 14:31:02 +0200

    Fix production mode in webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,11 +6,12 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
+    mode: 'production',
     plugins: [
         new HtmlWebpackPlugin({
             inject: false,
             template: './demo/index.ejs',
-            client_id: null
+            client_id: 'DIST_CLIENT_KEY' // to be replaced on demo file remote push
         }),
         new Dotenv({ path: './.env' })
     ]
